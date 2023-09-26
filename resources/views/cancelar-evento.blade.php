cef138b4befee8ef394f997ba4255e0259ab5af0
empanizado <luisvale4898@gmail.com>
2023-09-25 21:58:50 -0400

    algo

--- resources/views/cancelar-evento.blade.php
+++ resources/views/cancelar-evento.blade.php
@@ -1,6 +1,3 @@
 <form action="{{ route('eventos.cancelar', $evento->id) }}" method="POST">
-    @csrf
-    @method('PUT') <!-- Usar PUT en lugar de GET o POST si corresponde -->
-
     <button type="submit" onclick="return confirm('¿Estás seguro de que deseas cancelar este evento?')">Cancelar Evento</button>
 </form>
