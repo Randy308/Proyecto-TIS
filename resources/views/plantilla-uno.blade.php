ac4e4d526ff3beb37c2e385faa9e06f3e96afd7b
unknown <randygoitia7@gmail.com>
2023-10-11 21:14:59 -0400

    corrigiendo estilos

--- resources/views/plantilla-uno.blade.php
+++ resources/views/plantilla-uno.blade.php
@@ -136,18 +136,7 @@
                 </div>
             </div>
             <div class="card" id="tab2">
-                <div class="card">
-                    <textarea name="publicar" id="publicar-admin" cols="30" rows="10"></textarea>
-                    <button class="btn btn-primary" style="width: fit-content;">Publicar</button>
-                </div>
-
-
-                <div class="card">
-                    <span><b>usuario: </b>mensaje 1 ...</span>
-                </div>
-                <div class="card">
-                    <span><b>usuario: </b>mensaje 2 ...</span>
-                </div>
+                
             </div>
         </div>
     </div>
