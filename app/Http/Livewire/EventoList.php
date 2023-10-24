7300279bf5bcfbdbddf05bf3551a31ccb7a885e0
CarlosRevollo <carlos.revollo.villarroel@gmail.com>
2023-10-23 23:24:21 -0400

    cambio de lista de eventos

--- app/Http/Livewire/EventoList.php
+++ app/Http/Livewire/EventoList.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Evento;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 use DateTime;
 class EventoList extends Component
 
@@ -13,17 +14,42 @@ class EventoList extends Component
     protected $paginationTheme = 'bootstrap';
 
     use WithPagination;
-
+    public $filtroGestion;
     public $search ='';
     public $orderb = 0;
     public $filtroEstado = '';
     public $filtroCategoria = '';
-
+    public $firstTime = true;
     public function render()
     {
 
 
          $eventos = Evento::where('nombre_evento','LIKE',"%{$this->search}%");
+        if($this->firstTime){
+            $mes = date('n');
+            $anio = date('Y');
+            $gesActual = "";
+            if($mes <= 6){
+                $gesActual = "I";
+            }else{
+                $gesActual = "II";  
+            }
+            $this->filtroGestion = json_encode(['anio' => $anio, 'gestion' => $gesActual]);
+            $this->firstTime = false;
+        }
+        if($this->filtroGestion){
+            $filg = json_decode($this->filtroGestion);
+            $fI = $filg->anio;
+            $fF = $filg->anio;
+            if($filg->gestion == "I"){
+                $fI = $fI . "-01-01";
+                $fF = $fF ."-06-30";
+            }else{
+                $fI =  $fI . "-07-01";
+                $fF =  $fF . "-12-31";
+            }
+            $eventos->whereBetween('fecha_inicio', [ $fI,$fF]);
+        }else 
         if($this->filtroEstado){
             $eventos->where('estado',$this->filtroEstado);
 
@@ -36,10 +62,10 @@ class EventoList extends Component
 
          switch($this->orderb){
             case 0:
-                $eventos->orderBy('fecha_inicio', 'asc');
+                $eventos->orderBy('fecha_inicio', 'desc');
                 break;
             case 1:
-                $eventos->orderBy('fecha_inicio', 'desc');
+                $eventos->orderBy('fecha_inicio', 'asc');
                 break;
             case 2:
                 $eventos->orderBy('nombre_evento', 'asc');
@@ -58,9 +84,16 @@ class EventoList extends Component
             $evento->fecha_inicio = new DateTime($evento->fecha_inicio);
             $evento->fecha_fin = new DateTime($evento->fecha_fin);
         }
+        $gestiones = DB::table('eventos')
+        ->select(DB::raw('DISTINCT YEAR(fecha_inicio) as anio'), DB::raw('CASE WHEN MONTH(fecha_inicio) <= 6 THEN "I" ELSE "II" END as gestion'))
+        ->orderBy('anio', 'desc')
+        ->orderBy('gestion', 'desc')
+        ->get();
+
 
         return view('livewire.evento-list',  [
-            'eventos' => $eventos]);
+            'eventos' => $eventos,
+            'gestiones' => $gestiones]);
     }
 
     public function updatingSearch(){
