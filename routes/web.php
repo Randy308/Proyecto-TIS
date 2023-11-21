686ab29f01863b5a05ee02744fa05cb4df345473
unknown <randygoitia7@gmail.com>
2023-11-20 23:13:20 -0400

    agregando middleware para permisos

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,13 @@ Route::get('/', function () {
 
 Route::post('/home', [AjaxController::class, 'ajax'])->name('ajax');
 Route::get('/pruebas', [AjaxController::class, 'prueba'])->name('ajax-prueba');
-Route::get('/crear-evento', [EventoControlador::class, 'index'])->name('crear-evento')->middleware('checkRole:administrador,organizador');
-;
+Route::get('/crear-evento', [EventoControlador::class, 'index'])->name('crear-evento');;
 
-Route::post('/crear-evento', [EventoControlador::class, 'crearEvento'])->name('crear-evento')->middleware('checkRole:administrador,organizador');
+Route::post('/crear-evento', [EventoControlador::class, 'crearEvento'])->name('crear-evento');
 
 Route::get('/editar-evento', function () {
     return view('editar-evento');
-})->name('editar-evento')->middleware('checkRole:administrador,organizador');
+})->name('editar-evento');
 
 
 Route::post('/login', [AuthUser::class, 'store'])->name('iniciar.sesion.store');
@@ -44,7 +43,7 @@ Route::get('/evento/{id}', [EventoControlador::class, 'show'])->name('verEvento'
 Route::get('/lista-eventos', [EventoControlador::class, 'listaEventos'])->name('listaEventos');
 Route::put('/registroUsuario/{id}', [AsistenciaEventosController::class, 'create'])->name('registrar-evento-update');
 
-Route::delete('/eliminar/{user}/{evento}', [AsistenciaEventosController::class, 'destroy'])->name('user.delete')->middleware('checkRole:administrador,organizador');
+Route::delete('/eliminar/{user}/{evento}', [AsistenciaEventosController::class, 'destroy'])->name('user.delete');
 
 
 Route::get('/lista-usuarios', [UsuarioController::class, 'listaUsuarios'])->name('listaUsuarios');
@@ -60,29 +59,27 @@ Route::get('/misEventos', function () {
 })->name('misEventos')->middleware('checkRole:administrador,organizador,colaborador');
 
 
-Route::delete('/eliminarEvento/{user}/{evento}', [EventoControlador::class, 'destroy'])->name('evento.delete')->middleware('checkRole:administrador,organizador');
+Route::delete('/eliminarEvento/{user}/{evento}', [EventoControlador::class, 'destroy'])->name('evento.delete');
 
-Route::get('/editarBanner/{user}/{evento}', [EventoControlador::class, 'editBanner'])->name('evento.banner.edit')->middleware('checkRole:administrador,organizador');
-Route::get('/editarEvento/{user}/{evento}', [EventoControlador::class, 'edit'])->name('evento.edit')->middleware('checkRole:administrador,Organizador');
+Route::get('/editarBanner/{user}/{evento}', [EventoControlador::class, 'editBanner'])->name('evento.banner.edit');
+Route::get('/editarEvento/{user}/{evento}', [EventoControlador::class, 'edit'])->name('evento.edit');
 
-Route::put('/editarEvento/{user}/{evento}', [EventoControlador::class, 'update'])->name('evento.update')->middleware('checkRole:administrador,organizador');
+Route::put('/editarEvento/{user}/{evento}', [EventoControlador::class, 'update'])->name('evento.update');
 
-Route::put('/editarBanner/{user}/{evento}', [EventoControlador::class, 'updateBanner'])->name('evento.banner.update')->middleware('checkRole:administrador,organizador');
-
-
+Route::put('/editarBanner/{user}/{evento}', [EventoControlador::class, 'updateBanner'])->name('evento.banner.update');
 
 Route::put('/evento/{id}', [EventoControlador::class, 'guardarMap'])->name('updateMap');
 
 Route::post('/evento/{id}', [ImagenAuspiciadorController::class, 'store'])->name('guardarAus');
 Route::delete('/evento/{id}', [ImagenAuspiciadorController::class, 'destroy'])->name('eliminarAus');
 
-Route::get('/usuario/lista', [UsuarioController::class, 'listaUsuarios'])->name('listaUsuarios')->middleware('checkRole:administrador');
-Route::get('/usuario/crear', [UsuarioController::class, 'createForm'])->name('crearUsuario')->middleware('checkRole:administrador');
+Route::get('/usuario/lista', [UsuarioController::class, 'listaUsuarios'])->name('listaUsuarios');
+Route::get('/usuario/crear', [UsuarioController::class, 'createForm'])->name('crearUsuario');
 Route::post('/usuario/crear', [UsuarioController::class, 'store'])->name('crearUsuario.store');
-Route::get('/usuario/{id}', [UsuarioController::class, 'show'])->name('verUsuario')->middleware('checkRole:administrador');
-Route::get('/usuario/{id}/editar', [UsuarioController::class, 'editForm'])->name('editarUsuario')->middleware('checkRole:administrador');
+Route::get('/usuario/{id}', [UsuarioController::class, 'show'])->name('verUsuario');
+Route::get('/usuario/{id}/editar', [UsuarioController::class, 'editForm'])->name('editarUsuario');
 Route::put('/usuario/{id}/editar', [UsuarioController::class, 'edit'])->name('editarUsuario.edit');
-Route::get('/lista-usuarios', [UsuarioController::class, 'listaUsuarios'])->name('listaUsuarios')->middleware('checkRole:administrador');
+Route::get('/lista-usuarios', [UsuarioController::class, 'listaUsuarios'])->name('listaUsuarios');
 
 Route::post('/guardar-elementos/{evento}', [ElementosBannerController::class, 'store'])->name('crear-elementos-banner');
 
@@ -103,15 +100,12 @@ Route::get('/acceso-denegado', function () {
     return view('acceso-denegado');
 })->name('acceso-denegado');
 
-Route::put('/editarEstado/{user}/{evento}', [EventoControlador::class, 'updateEstado'])->name('evento.state.update')->middleware('checkRole:administrador,Organizador');
+Route::put('/editarEstado/{user}/{evento}', [EventoControlador::class, 'updateEstado'])->name('evento.state.update');
 
-Route::get('/editarBanner/{user}/{evento}', [EventoControlador::class, 'editBanner'])->name('evento.banner.edit')->middleware('checkRole:administrador,Organizador');
-Route::put('/editarBanner/{user}/{evento}', [EventoControlador::class, 'updateBanner'])->name('evento.banner.update')->middleware('checkRole:administrador,Organizador');
+Route::get('/editarBanner/{user}/{evento}', [EventoControlador::class, 'editBanner'])->name('evento.banner.edit');
+Route::put('/editarBanner/{user}/{evento}', [EventoControlador::class, 'updateBanner'])->name('evento.banner.update');
 
-Route::get('/auspiciadores', [AuspiciadorController::class, 'index'])->name('auspiciadores-index');
 
-Route::post('/auspiciadores', [AuspiciadorController::class, 'store'])->name('auspiciador.store');
-Route::post('/auspiciadores', [AuspiciadorController::class, 'store'])->name('auspiciador.store');
 
 Route::get('/editarPerfil', function () {
     return view('editar-perfil');
@@ -138,10 +132,16 @@ Route::get('/asignarPermiso/{role}', [PermisoController::class, 'edit'])
 Route::put('/asignarPermiso/{role}', [PermisoController::class, 'update'])
     ->name('asignarPermiso.update');
 
-
-
-Route::get('/perfil/{id}/editar', [UsuarioController::class, 'editUser'])->name('editUser')->middleware('checkRole:administrador');
+Route::get('/perfil/{id}/editar', [UsuarioController::class, 'editUser'])->name('editUser');
 Route::put('/perfil/{id}/editar', [UsuarioController::class, 'update'])->name('editUser.update');
 
 Route::delete('/eliminar/{user}', [UsuarioController::class, 'destroy'])
     ->name('user.delete');
+
+Route::group(['middleware' => ['can:admin.crear-auspiciador']], function () {
+
+    Route::get('/auspiciadores', [AuspiciadorController::class, 'index'])->name('auspiciadores-index');
+
+    Route::post('/auspiciadores', [AuspiciadorController::class, 'store'])->name('auspiciador.store');
+    Route::post('/auspiciadores', [AuspiciadorController::class, 'store'])->name('auspiciador.store');
+});
