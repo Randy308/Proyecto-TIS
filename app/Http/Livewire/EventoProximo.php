5767416d7dad7913dae8681b30caf1a2df6218ff
unknown <randygoitia7@gmail.com>
2023-11-21 16:32:42 -0400

    agregando afiche proximo

--- app/Http/Livewire/EventoProximo.php
+++ app/Http/Livewire/EventoProximo.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Evento;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class EventoProximo extends Component
@@ -14,7 +15,18 @@ class EventoProximo extends Component
             ->where('Estado', '=', 'Activo')
             ->orderBy('fecha_inicio', 'asc')
             ->first();
-
-        return view('livewire.evento-proximo', compact('evento'));
+        $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+        $fecha = Carbon::parse($evento->fecha_fin);
+        $fecha_inicial = Carbon::parse($evento->fecha_inicio);
+        $mes = $meses[($fecha->format('n')) - 1];
+        $mes_inicial = $meses[($fecha_inicial->format('n')) - 1];
+        //$miFechaInicial;
+        if ($mes == $mes_inicial) {
+            $miFechaInicial = $fecha_inicial->format('d') . ' y ';
+        } else {
+            $miFechaInicial = $fecha_inicial->format('d') . ' de ' . $mes_inicial . ' - ';
+        }
+        $mifechaFinal = $miFechaInicial . $fecha->format('d') . ' de ' . $mes . ' ' . $fecha->format('Y');
+        return view('livewire.evento-proximo', compact('evento','mifechaFinal'));
     }
 }
