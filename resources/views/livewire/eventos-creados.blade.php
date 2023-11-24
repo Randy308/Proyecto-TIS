184909cebeed80d39146de6ce57ce7c8657183e6
unknown <randygoitia7@gmail.com>
2023-11-24 13:10:08 -0400

    cambiando configuracion en lista de mis evnetos

--- resources/views/livewire/eventos-creados.blade.php
+++ resources/views/livewire/eventos-creados.blade.php
@@ -10,6 +10,7 @@
                         <th>Nombre del Evento</th>
                         <th>Estado del Evento</th>
                         <th>Banner del Evento</th>
+
                         <th></th>
                         <th></th>
                         <th></th>
@@ -24,53 +25,70 @@
                             <td>{{ $evento->nombre_evento }}</td>
                             <td>{{ $evento->estado }}</td>
                             <td><img src="{{ $evento->direccion_banner }}" width="170px" alt="{{ $evento->Titulo }}"></td>
-                            <td width="10px">
-                                <form
-                                    action="{{ route('evento.banner.edit', ['user' => auth()->user(), 'evento' => $evento]) }}"
-                                    method="get">
-                                    <button class="btn btn-info" type="submit">Editar Banner</button>
+                            @if (strtoupper($evento->estado) == 'BORRADOR')
+                                <td width="10px">
+                                    <form
+                                        action="{{ route('evento.banner.edit', ['user' => auth()->user(), 'evento' => $evento]) }}"
+                                        method="get">
+                                        <button class="btn btn-info" type="submit">Editar <br> Banner</button>
 
-                                </form>
-
-
-                            </td>
-                            <td width="10px">
-                                <form
-                                    action="{{ route('evento.delete', ['user' => auth()->user(), 'evento' => $evento]) }}"
-                                    method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger" type="submit" {{$evento->estado != 'Borrador' ? 'disabled' : ''}}>Cancelar</button>
-
-                                </form>
-
-
-                            </td>
-                            <td width="10px">
-                                @if ($evento->estado == 'Activo')
-                                <button class="btn btn-secondary" disabled>Publicado</button>
-                                @else
-                                    <form id="FormPublicar"
-                                        action="{{ route('evento.state.update', ['user' => auth()->user(), 'evento' => $evento]) }}"
-                                        method="post">
-                                        @csrf
-                                        @method('PUT')
-                                        <button class="btn btn-warning boton-publicar"
-                                        type="button">Publicar</button>
                                     </form>
 
-                                @endif
+
+                                </td>
+                                <td width="10px">
+                                    <form
+                                        action="{{ route('evento.delete', ['user' => auth()->user(), 'evento' => $evento]) }}"
+                                        method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="submit"
+                                            {{ $evento->estado != 'Borrador' ? 'disabled' : '' }}>Cancelar</button>
+
+                                    </form>
 
 
-                            </td>
-                            <td width="10px">
-                                <form
-                                    action="{{ route('evento.edit', ['user' => auth()->user(), 'evento' => $evento]) }}"
-                                    method="get">
-                                    <button type="submit" class="btn btn-success">Editar</button>
-                                </form>
+                                </td>
+                                <td width="10px">
+                                    @if ($evento->estado == 'Activo')
+                                        <button class="btn btn-secondary" disabled>Publicado</button>
+                                    @else
+                                        <form id="FormPublicar"
+                                            action="{{ route('evento.state.update', ['user' => auth()->user(), 'evento' => $evento]) }}"
+                                            method="post">
+                                            @csrf
+                                            @method('PUT')
+                                            <button class="btn btn-warning boton-publicar"
+                                                type="button">Publicar</button>
+                                        </form>
+                                    @endif
 
-                            </td>
+
+                                </td>
+                                <td width="10px">
+                                    <form
+                                        action="{{ route('evento.edit', ['user' => auth()->user(), 'evento' => $evento]) }}"
+                                        method="get">
+                                        <button type="submit" class="btn btn-success">Editar</button>
+                                    </form>
+
+                                </td>
+                            @else
+
+                                <td width="10px">
+
+                                </td>
+                                <td width="10px">
+
+                                </td>
+                                <td width="10px">
+
+                                </td>
+                                <td width="10px">
+                                    <button class="btn btn-secondary" disabled>Publicado</button>
+                                </td>
+                            @endif
+
 
                         </tr>
                     @endforeach
