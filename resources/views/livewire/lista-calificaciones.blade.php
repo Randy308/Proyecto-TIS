8c39bbc9997840417a48049efffc0fae68339a92
unknown <randygoitia7@gmail.com>
2023-12-14 11:54:24 -0400

    corrgiendo vista de calificacion

--- resources/views/livewire/lista-calificaciones.blade.php
+++ resources/views/livewire/lista-calificaciones.blade.php
@@ -27,79 +27,88 @@
 
 
     </div>
-    <div class="row p-4">
-        <table class="table table-bordered data-table table-responsive-sm">
-            <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Nota minima</th>
-                    <th>Nota maxima</th>
-
-                    <th>Acción</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($calificaciones as $calificacion)
+    @if ($calificaciones->count())
+        <div class="row p-4">
+            <table class="table table-bordered data-table table-responsive-sm">
+                <thead>
                     <tr>
-                        <td>
-                            {{ $calificacion->nombre }}
-                        </td>
-                        <td>
-                            {{ $calificacion->nota_minima_aprobacion }}</a>
-                        </td>
-                        <td >
-                            {{ $calificacion->nota_maxima }}</a>
-                        </td>
+                        <th>Nombre</th>
+                        <th>Nota minima</th>
+                        <th>Nota maxima</th>
 
-
-
-                        <td class="d-flex justify-content-between">
-                            @if (strtoupper($evento->modalidad) == 'GRUPAL')
-                                <a href="{{ route('calificar.grupos', ['evento_id' => $evento->id, 'calificacion_id' => $calificacion->id]) }}"
-                                    class="btn btn-link btn-sm" type="button">Visualizar grupos</a>
-                            @else
-                                <a href="{{ route('calificar.participantes', ['evento_id' => $evento->id, 'calificacion_id' => $calificacion->id]) }}"
-                                    class="btn btn-link btn-sm" type="button">Visualizar</a>
-                            @endif
-                            @if ($loop->last)
-                                <form  action="{{ route('borrar.calificacion', ['calificacion_id' => $calificacion->id]) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button" class="btn btn-danger boton-eliminar"><i
-                                            class="bi bi-trash"></i></button>
-                                </form>
-                            @endif
-
-                        </td>
+                        <th>Acción</th>
                     </tr>
-                @endforeach
-
-            </tbody>
-        </table>
-        <script>
-            $(".boton-eliminar").on("click", function(e) {
-                e.preventDefault();
-                if (confirm("¿Está seguro de que deseas eliminar la calificacion?")) {
-                    var form = $(this).parents('form:first');
-                    console.log('enviando form')
-                    form.submit();
-                }
-            });
-        </script>
-    </div>
-    @if ($existe == 0)
-        <div class="d-flex justify-content-end">
-            <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#promedioModal">
-                Finalizar calificaciones
-            </button>
-            @if (strtoupper($evento->modalidad) == 'GRUPAL')
-                @include('layouts.modal-crear-promedio-grupos', ['evento_id' => $evento_id])
-            @else
-                @include('layouts.modal-crear-promedio', ['evento_id' => $evento_id])
-            @endif
+                </thead>
+                <tbody>
+                    @foreach ($calificaciones as $calificacion)
+                        <tr>
+                            <td>
+                                {{ $calificacion->nombre }}
+                            </td>
+                            <td>
+                                {{ $calificacion->nota_minima_aprobacion }}</a>
+                            </td>
+                            <td>
+                                {{ $calificacion->nota_maxima }}</a>
+                            </td>
 
 
+
+                            <td class="d-flex justify-content-between">
+                                @if (strtoupper($evento->modalidad) == 'GRUPAL')
+                                    <a href="{{ route('calificar.grupos', ['evento_id' => $evento->id, 'calificacion_id' => $calificacion->id]) }}"
+                                        class="btn btn-link btn-sm" type="button">Visualizar grupos</a>
+                                @else
+                                    <a href="{{ route('calificar.participantes', ['evento_id' => $evento->id, 'calificacion_id' => $calificacion->id]) }}"
+                                        class="btn btn-link btn-sm" type="button">Visualizar</a>
+                                @endif
+                                @if ($loop->last)
+                                    <form
+                                        action="{{ route('borrar.calificacion', ['calificacion_id' => $calificacion->id]) }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button" class="btn btn-danger boton-eliminar"><i
+                                                class="bi bi-trash"></i></button>
+                                    </form>
+                                @endif
+
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+            <script>
+                $(".boton-eliminar").on("click", function(e) {
+                    e.preventDefault();
+                    if (confirm("¿Está seguro de que deseas eliminar la calificacion?")) {
+                        var form = $(this).parents('form:first');
+                        console.log('enviando form')
+                        form.submit();
+                    }
+                });
+            </script>
+        </div>
+        @if ($existe == 0)
+            <div class="d-flex justify-content-end">
+                <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#promedioModal">
+                    Finalizar calificaciones
+                </button>
+                @if (strtoupper($evento->modalidad) == 'GRUPAL')
+                    @include('layouts.modal-crear-promedio-grupos', ['evento_id' => $evento_id])
+                @else
+                    @include('layouts.modal-crear-promedio', ['evento_id' => $evento_id])
+                @endif
+
+
+            </div>
+        @endif
+    @else
+        <div class="card-body">
+            <strong>No hay registros calificaciones</strong>
         </div>
     @endif
+
 
 </div>
