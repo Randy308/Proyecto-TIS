135c7581500a5b7317689de38c561268b4aee3f2
CarlosRevollo <carlos.revollo.villarroel@gmail.com>
2023-12-21 02:25:01 -0400

    cambios algunos

--- app/Mail/NotificacionEventoEmail.php
+++ app/Mail/NotificacionEventoEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NotificacionEventoEmail extends Mailable implements ShouldQueue
+class NotificacionEventoEmail extends Mailable 
 {
     use Queueable, SerializesModels;
 
